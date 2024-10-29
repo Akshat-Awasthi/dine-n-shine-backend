42cf15d84acc7a2275b9282fdedc1cddbea1d569
Akshat Awasthi <aakshat444@gmail.com>
2024-10-30 01:42:52 +0530

    na

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ load_dotenv()
 
 app = FastAPI()
 
-MONGO_USER = os.getenv("MONGO_USER", "dineAdmin")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+# MONGO_USER = os.getenv("MONGO_USER", "dineAdmin")
+# MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_DETAILS = "mongodb+srv://dineAdmin:dine-n-shine@dine-n-shine-cluster.1b1o3.mongodb.net/?retryWrites=true&w=majority&appName=dine-n-shine-cluster"
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
