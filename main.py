e35bb879946a406c08d8ac4a0f6c8a322176c76b
Akshat Awasthi <aakshat444@gmail.com>
2024-11-04 20:06:48 +0530

    added service apis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from typing import List
@@ -13,12 +13,19 @@ app = FastAPI()
 
 MONGO_USER = os.getenv("MONGO_USER", "dineAdmin")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_DETAILS = f"mongodb+srv://dineAdmin:<dine-n-shine>@dine-n-shine-cluster.1b1o3.mongodb.net/?retryWrites=true&w=majority&appName=dine-n-shine-cluster"
+MONGO_DETAILS = f"mongodb+srv://dineAdmin:dine-n-shine@dine-n-shine-cluster.1b1o3.mongodb.net/?retryWrites=true&w=majority&appName=dine-n-shine-cluster"
 
+try:
+    client = AsyncIOMotorClient(MONGO_DETAILS)
+    client.admin.command('ping')
+    print("MongoDB connection successful!")
+except Exception as e:
+    print("MongoDB connection error:", e)
 
-client = AsyncIOMotorClient(MONGO_DETAILS)
+# client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client["dine-n-shine"]
 order_collection = database["orders"]
+service_collection = database["services"]
 
 origins = [
     "http://localhost:5173",
@@ -52,11 +59,59 @@ def order_serializer(order) -> dict:
             for item in order.get("items", [])
         ],
     }
+def service_serializer(service) -> dict:
+    return{
+        "id":str(service.get("_id")),
+        "img":service.get("img"),
+        "title":service.get("title"),
+        "owner":service.get("owner"),
+        "cost":service.get("cost"),
+        "date_added":service.get("date_added"),
+        "distription":[
+            {
+                "point":distription.get("point")
+            }
+            for distription in service.get("distription",[])
+        ],
+        "menu":{
+            "lunch":[
+                {
+                    "dish": lunch_item.get("dish")
+                }
+                for lunch_item in service.get("menu",{}).get("lunch",[])
+            ],
+            "dinner":[
+                {
+                    "dish": dinner_item.get("dish")
+                }
+                for dinner_item in service.get("menu",{}).get("dinner",[])
+            ]
+        }
+    }
 
 @app.get("/")
 async def home():
     return "welcome to dine-n-shine"
 
+# Service APIs
+@app.get("/get_services")
+async def get_services():
+    services = await service_collection.find().to_list(100)
+    service_list = [service_serializer(service) for service in services]
+    return {"services":service_list}
+    
+@app.get("/service_by_id/{_id}")
+async def service_by_id(_id: str):
+    if not ObjectId.is_valid(_id):
+        raise HTTPException(status_code=400, detail="Invalid service ID")
+    selected_service = await service_collection.find_one({"_id": _id})
+    if selected_service:
+        return service_serializer(selected_service)
+    else:
+        raise HTTPException(status_code=404, detail="Service not found")
+
+
+# Order APIs
 @app.get("/orders")
 async def get_orders():
     orders = await order_collection.find().to_list(100)
@@ -64,6 +119,16 @@ async def get_orders():
     new_orders = [order for order in order_list if order["status"] == "Not Paid"]
     
     return {"orders": order_list, "newOrders": new_orders}
+
+@app.get("/order_by_id/{_id}")
+async def order_by_id(_id: str):
+    if not ObjectId.is_valid(_id):
+        raise HTTPException(status_code=400, detail="Invalid order ID")
+    selected_order = await order_collection.find_one({"_id": ObjectId(_id)})
+    if selected_order:
+        return order_serializer(selected_order)
+    else:
+        raise HTTPException(status_code=404, detail="Order not found")
 
 @app.post("/create_order")
 async def create_orders(order: Order):
@@ -76,7 +141,7 @@ async def create_orders(order: Order):
     else:
         raise HTTPException(status_code=400, detail="Order creation failed")
     
-@app.put("/update_order")
+@app.put("/update_order/{_id}")
 async def update_orders(order: Order, _id: str):
     if not ObjectId.is_valid(_id):
         raise HTTPException(status_code=400, detail="Invalid order ID")
@@ -88,6 +153,41 @@ async def update_orders(order: Order, _id: str):
         return order_serializer(updated_result)
     else:
         raise HTTPException(status_code=404, detail="Order not found or no fields to update")
+    
+@app.delete("/delete_order")
+async def delete_order(_id : str):
+    if not ObjectId.is_valid(_id):
+        raise HTTPException(status_code=400, detail="Invalid order ID")
+    
+    selected_order = await order_collection.find_one({"_id":ObjectId(_id)})
+    if selected_order is None:
+        raise HTTPException(status_code=404,detail="order not found")
+    deleted_order = await order_collection.delete_one({"_id":ObjectId(_id)})
+
+    if deleted_order.deleted_count==1:
+        return{"message":"Order deleted successfully"}
+    else:
+        raise HTTPException(status_code=500,detail="Failed to delete order")
+    
+@app.get("/search_orders")
+async def search_orders(query: str = Query(...)):
+    try:
+        if ObjectId.is_valid(query):
+            matching_orders = await order_collection.find({"_id": ObjectId(query)}).to_list(100)
+        else:
+            matching_orders = await order_collection.find(
+                {"token": {"$regex": f"^{query}", "$options": "i"}}
+            ).to_list(10)
+
+        if not matching_orders:
+            raise HTTPException(status_code=404, detail="No matching orders found")
+        
+        return [order_serializer(order) for order in matching_orders]
+
+    except Exception as e:
+        print(f"Search error: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
 
 if __name__ == "__main__":
     import uvicorn
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from datetime import datetime
 import uuid
 
 
@@ -19,8 +20,19 @@ class Order(BaseModel):
     Amount: str
     Remaining: str
     items: List[OrderItem]
-
-
 class OrdersResponse(BaseModel):
     orders: List[Order]
     newOrders: List[Order]
+
+class DiscriptionPoint(BaseModel):
+    point:str
+class Service(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    img:str
+    title:str
+    owner:str
+    cost:str
+    distription:Optional[List[dict]]
+    date_added:datetime=Field(default_factory=datetime.utcnow)
+
+    
